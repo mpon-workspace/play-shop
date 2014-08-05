dc827ea665dad5c8ab5d47cd3d1130b12ff989a5
Masato OSHIMA <qqw75pn9@gmail.com>
2014-08-05 23:20:48 +0900

    implemented login authenticate

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -10,6 +10,7 @@ import models.*;
 
 public class Application extends Controller {
 
+	@Security.Authenticated(Secured.class)
     public static Result index() {
         return ok(index.render());
     }
@@ -18,11 +19,18 @@ public class Application extends Controller {
     	return ok(login.render(Form.form(User.class)));
     }
 
+    public static Result logout() {
+    	session().clear();
+    	return redirect(routes.Application.login());
+    }
+
     public static Result authenticate() {
     	Form<User> loginForm = Form.form(User.class).bindFromRequest();
     	if (loginForm.hasErrors()) {
     		return badRequest(login.render(loginForm));
     	}
+    	session().clear();
+        session("name", loginForm.get().name);
     	return redirect(routes.Application.index());
     }
 
